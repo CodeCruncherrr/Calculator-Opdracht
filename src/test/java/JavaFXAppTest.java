99b6404536dec8245f30dc3ea2ce18b3b2da2f79
harryray26 <rayneesh26@gmail.com>
2024-11-12 18:33:49 +0100

    testen toegevoed voor optellen, vermenigvuldigen en delen

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -3,25 +3,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class JavaFXAppTest {
     @Test
-    void multiplytest() {
-        JavaFXApp java = new JavaFXApp();
-        int result = java.computeMultiply(2,3);
-        assertEquals(6,result);
-    }
-
-    @Test
-    void computeDividetest() {
-        JavaFXApp java = new JavaFXApp();
-        int result = java.computeDivide(8,4);
-        assertEquals(2,result);
-    }
-
-    @Test
+        //testen voor optellen
     void computeAddtest() {
         JavaFXApp java = new JavaFXApp();
         int result = java.computeAdd(8,4);
         assertEquals(12,result);
     }
-
+    @Test
+        //testen voor vermenigvuldigen
+    void multiplytest() {
+        JavaFXApp java = new JavaFXApp();
+        int result = java.computeMultiply(2,3);
+        assertEquals(6,result);
+    }
+    @Test
+        //testen voor delen
+    void computeDividetest() {
+        JavaFXApp java = new JavaFXApp();
+        int result = java.computeDivide(8,4);
+        assertEquals(2,result);
+    }
 }
 
